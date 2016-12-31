70d1479621dc923454c1d11e9a2c174d4b0377f4
Alexander Ivkin <ivkin@almworks.com>
2016-12-31 19:36:14 +0700

    Implemented first main page draft

---
+++ static/js/main.js
@@ -0,0 +1,22 @@
+(function($) {
+  $(function () {
+    update();
+    $(window).resize(update);
+  });
+
+  function update() {
+    updateManHeight();
+    updateGraphPosition();
+  }
+
+  function updateManHeight() {
+    var h = $('#services').outerHeight();
+    $('.man').height(h);
+  }
+
+  function updateGraphPosition() {
+    var w = $(window).width();
+    var p = Math.round(w / 2) - 270 + 'px 170px';
+    $('.graph').css('background-position', p);
+  }
+})(jQuery);
